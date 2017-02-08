bb941e59d50e57038ed519292129b7e459d5ff47
quanglh <quanglh@bkav.com>
2017-02-08 10:51:29 +0700

    QuangLH: khoi tao project theo server 10.2.53.28
    Change-Id: If743b560a0ab4b516cf4a562ff9c66ffcff8f3d6

---
+++ src/com/android/calculator2/CalculatorPadOperatorLayout.java
@@ -0,0 +1,150 @@
+
+package com.android.calculator2;
+
+/*@trungnvd
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bkav.calculator2.R;
+
+/**
+ * A layout that places children in an evenly distributed grid based on the
+ * specified {@link android.R.attr#columnCount} and
+ * {@link android.R.attr#rowCount} attributes.
+ */
+@SuppressLint("NewApi")
+public class CalculatorPadOperatorLayout extends ViewGroup {
+
+    private int mRowCount;
+    private int mColumnCount;
+    private Context mContext;
+
+    public CalculatorPadOperatorLayout(Context context) {
+        this(context, null);
+        mContext = context;
+    }
+
+    public CalculatorPadOperatorLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+        mContext = context;
+    }
+
+    public CalculatorPadOperatorLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mContext = context;
+        final TypedArray a = context.obtainStyledAttributes(attrs,
+                new int[] {
+                        android.R.attr.rowCount, android.R.attr.columnCount
+                }, defStyle, 0);
+        mRowCount = a.getInt(0, 1);
+        mColumnCount = a.getInt(1, 1);
+
+        a.recycle();
+    }
+
+    @Override
+    public boolean shouldDelayChildPressedState() {
+        return false;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        final int paddingLeft = getPaddingLeft();
+        final int paddingRight = getPaddingRight();
+        final int paddingTop = getPaddingTop();
+        final int paddingBottom = getPaddingBottom();
+
+        final boolean isRTL = getLayoutDirection() == LAYOUT_DIRECTION_RTL;
+        final int columnWidth =
+                Math.round((float) (right - left - paddingLeft - paddingRight)) / mColumnCount;
+        int rowHeight =
+                Math.round((float) (bottom - top - paddingTop - paddingBottom)) / mRowCount;
+
+        int rowIndex = 0, columnIndex = 0;
+
+        // Bkav QuangLH:
+        float addedHeight = mContext.getResources().getDimension(R.dimen.padding_button_more);
+        final int rowHeightLast = (int) (((rowHeight * 6) -(rowHeight + addedHeight)) / (mRowCount - 1));
+
+        for (int childIndex = 0; childIndex < getChildCount(); ++childIndex) {
+            final View childView = getChildAt(childIndex);
+            if (childView.getVisibility() == View.GONE) {
+                continue;
+            }
+
+            final MarginLayoutParams lp = (MarginLayoutParams) childView.getLayoutParams();
+           
+            final int childTop = paddingTop + lp.topMargin + rowIndex * rowHeight;
+            final int childBottom = childTop - lp.topMargin - lp.bottomMargin + rowHeight;
+            //BKAV trungnvd
+            final int childTopLast = paddingTop + lp.topMargin + lp.bottomMargin+ ((rowIndex - 1) * rowHeightLast) + (int) (addedHeight) + rowHeight;
+            final int childBottomLast = childTopLast - lp.topMargin  + rowHeightLast;
+            
+            
+            final int childLeft = paddingLeft + lp.leftMargin +
+                    (isRTL ? (mColumnCount - 1) - columnIndex : columnIndex) * columnWidth;
+            final int childRight = childLeft - lp.leftMargin - lp.rightMargin + columnWidth;
+
+            final int childWidth = childRight - childLeft;
+            final int childHeight = childBottom - childTop;
+            if (childWidth != childView.getMeasuredWidth() ||
+                    childHeight != childView.getMeasuredHeight()) {
+                childView.measure(
+                        MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.EXACTLY),
+                        MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.EXACTLY));
+            }
+         
+            if (rowIndex == 0) {
+                childView
+                        .layout(childLeft, childTop, childRight, (int) (childBottom + addedHeight));
+                System.out.println("trungnvbd check " + rowHeight + "bttom = " +(childBottom + addedHeight));
+            } 
+            else {
+                childView.layout(childLeft, childTopLast, childRight, childBottomLast);
+                System.out.println("trungnvbd check rowHeightLast" + paddingTop + "top = " + childTopLast + "botto=" +childBottomLast);
+            }
+
+            rowIndex = (rowIndex + (columnIndex + 1) / mColumnCount) % mRowCount;
+            columnIndex = (columnIndex + 1) % mColumnCount;
+        }
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new MarginLayoutParams(p);
+    }
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof MarginLayoutParams;
+    }
+}
