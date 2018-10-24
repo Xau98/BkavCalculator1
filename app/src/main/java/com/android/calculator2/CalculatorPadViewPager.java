f4fb12e7c0601cab2a5272f97ad6d0fc55805454
thanhngd <thanhngd@bkav.com>
2018-10-24 19:23:43 +0700

    B2018: Caculator: Hoan thanh fix bug lich su trong' khi hien page 0 van click button 123.. cua page 1
    Change-Id: Ifed5d6ca3e50fd8546518498c7d2e56762ea638f

--- app/src/main/java/com/android/calculator2/CalculatorPadViewPager.java
+++ app/src/main/java/com/android/calculator2/CalculatorPadViewPager.java
@@ -22,9 +22,12 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bkav.calculator2.R;
 
@@ -69,31 +72,43 @@ public class CalculatorPadViewPager extends ViewPager {
         }
     };
 
-    private final OnPageChangeListener mOnPageChangeListener = new SimpleOnPageChangeListener() {
-        private void recursivelySetEnabled(View view, boolean enabled) {
-            if (view instanceof ViewGroup) {
-                final ViewGroup viewGroup = (ViewGroup) view;
-                for (int childIndex = 0; childIndex < viewGroup.getChildCount(); ++childIndex) {
-                    recursivelySetEnabled(viewGroup.getChildAt(childIndex), enabled);
-                }
-            } else {
-                // Bkav ThanhNgD: setEnabled(...) xu li kha nang Touchables( co' the cham.) cua view
-                // false -> vo hieu hoa' Touchables, true -> bat Touchables
-                view.setEnabled(enabled);
+    public void recursivelySetEnabled(View view, boolean enabled) {
+        if (view instanceof ViewGroup) {
+            // Bkav ThanhNgD: Can` chuyen view thanh` viewGrop vi`: view chuyen` vao` gom` nhieu` child view, ma`
+            // method setEnable() o duoi' chi thuc hien dc voi' cac base view nhu TextView, Button...
+            final ViewGroup viewGroup = (ViewGroup) view;
+            // Thuc hien de quy lai method nay` voi' tat' ca cac' child view cua viewGroup
+            // Method nay` se chay vao` else{} chu' k vao` day nua~ vi` viewGroup.getChildAt(childIndex)
+            // luc' nay` la` base view -> (view instanceof ViewGroup) la` false
+            for (int childIndex = 0; childIndex < viewGroup.getChildCount(); ++childIndex) {
+                recursivelySetEnabled(viewGroup.getChildAt(childIndex), enabled);
             }
+        } else {
+            // Bkav ThanhNgD: setEnabled(...) xu li kha nang Touchables( co' the cham.) cua view
+            // false -> vo hieu hoa' Touchables, true -> bat Touchables
+            view.setEnabled(enabled);
         }
+    }
 
+    private final OnPageChangeListener mOnPageChangeListener = new SimpleOnPageChangeListener() {
         @Override
         public void onPageSelected(int position) {
             if (getAdapter() == mStaticPagerAdapter) {
+                TextView emptyElement = (TextView) getChildAt(0).findViewById(R.id.emptyElement);
                 for (int childIndex = 0; childIndex < getChildCount(); ++childIndex) {
-                    // Only enable subviews of the current page.
-
-                    //Bkav ThanhNgD: childIndex == position -> true
-                    // Neu' la` childIndex == position thi` page dang chon moi' click dc, page khac'
-                    // du` co' hien cung k the click vi`  setEnabled() = false
-                    // Con` la` true thi` neu' view dc hien tren windown thi` co the click
-                    recursivelySetEnabled(getChildAt(childIndex), true);
+                    // Xu ly bug khi lich su trong'(emptyElement dang VISIBLE ) van click button 123... cua page 1
+                    if(emptyElement != null && getCurrentItem() == 0 && emptyElement.getVisibility() == VISIBLE){
+                        CalculatorNumericPadLayout calculatorNumericPadLayout
+                                = (CalculatorNumericPadLayout) getChildAt(1).findViewById(R.id.pad_numeric);
+                        recursivelySetEnabled( calculatorNumericPadLayout, false);
+                    }
+                    else {
+                        //Bkav ThanhNgD: childIndex == position -> true
+                        // Neu' la` childIndex == position thi` page dang chon moi' click dc, page khac'
+                        // du` co' hien cung k the click vi`  setEnabled() = false
+                        // Con` la` true thi` neu' view dc hien tren windown thi` co the click
+                        recursivelySetEnabled( getChildAt(childIndex), true);
+                    }
                 }
             }
         }
@@ -106,6 +121,10 @@ public class CalculatorPadViewPager extends ViewPager {
                 mIScrollViewPager.onScroll(position, positionOffset, positionOffsetPixels);
         }
     };
+
+    public OnPageChangeListener getmOnPageChangeListener(){
+        return mOnPageChangeListener;
+    }
 
     private final PageTransformer mPageTransformer = new PageTransformer() {
         @Override
